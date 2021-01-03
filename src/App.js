2b14751f0db52415ac857cabae9384b24c690a81
iamabhishektiwari <abhishek.tiwari.cer15@itbhu.ac.in>
2021-01-03 12:32:05 +0530

    feat: navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/Homepage";
 
+const Hatpage = () => {
+  return (
+    <div>
+      <h1>Hatspage</h1>
+    </div>
+  );
+};
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={Hatpage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
